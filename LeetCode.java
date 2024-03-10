2a5f4ef07b716350e752ea2160a58cb9f6f5f384
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-10 23:47:55 +0530

    Update LeetCode.java

--- LeetCode.java
+++ LeetCode.java
@@ -21,3 +21,34 @@ class Solution {
         return false;
     }
 }
+//Intersection of two integer Array:
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        // Sort both arrays to ensure duplicate elements are adjacent
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int[] result = new int[Math.min(nums1.length, nums2.length)];
+        int index = 0;
+        
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                // Check if the current element is not a duplicate
+                if (index == 0 || result[index - 1] != nums1[i]) {
+                    result[index++] = nums1[i];
+                }
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        // Trim the result array to remove unused elements
+        return Arrays.copyOf(result, index);
+    }
+}
