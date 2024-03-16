1807bbe9303d585779e9234a3e790afb16a0be16
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-16 23:35:18 +0530

    Update LeetCode.java

--- LeetCode.java
+++ LeetCode.java
@@ -184,3 +184,21 @@ class Solution {
         return result;
     }
 }
+
+//
+Find square root without using pow() :
+
+class Solution {
+    public int mySqrt(int x) {
+        int l = 0, r = x;
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (mid > x / mid) {
+                r = mid - 1;
+            } else {
+                l = mid;
+            }
+        }
+        return l;
+    }
+}
