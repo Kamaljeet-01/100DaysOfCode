f221eb9d34112eb6e66cb361c195fe1cf4d74c0e
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-18 23:05:03 +0530

    Update LeetCode.java
    Sorting colors

--- LeetCode.java
+++ LeetCode.java
@@ -211,3 +211,20 @@ class Solution {
    return n > 0 && (n & (n - 1)) == 0 && (n & 0xAAAAAAAA) == 0;
     }
 }
+
+//Sort Colors :
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int temp=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]) {
+                    temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+    }
+}
