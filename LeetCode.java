49a0af393cf9830ff8b90fbd665b414cdaa7c764
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-20 22:48:37 +0530

    Update LeetCode.java

--- LeetCode.java
+++ LeetCode.java
@@ -264,3 +264,18 @@ class Solution {
         return arr[0];
     }
 }
+
+//Remove element:
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+    int k = 0 ; // Initialize the counter for elements not equal to val
+    for (int i=0;i<nums.length;i++){
+        if (nums[i] != val){
+            nums[k] = nums[i];
+            k += 1;
+            }
+    }
+    return k;
+    }
+}
