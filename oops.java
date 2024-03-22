251c2ab15ac58cd91f886c6713d2e759eb6036a6
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-22 22:15:04 +0530

    Create oops.java

---
+++ oops.java
@@ -0,0 +1,22 @@
+//Learning OOPS
+
+public class oops { 
+	
+	static String Employee_name; 
+	static float Employee_salary; 
+
+	static void set(String n, float p) { 
+		Employee_name = n; 
+		Employee_salary = p; 
+	} 
+
+	static void get() { 
+		System.out.println("Employee name is: " +Employee_name ); 
+		System.out.println("Employee CTC is: " + Employee_salary); 
+	} 
+
+	public static void main(String args[]) { 
+		GFG.set("Rathod Avinash", 10000.0f); 
+		GFG.get(); 
+	} 
+} 
