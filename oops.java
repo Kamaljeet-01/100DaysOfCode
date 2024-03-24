e7a86ae79a1eba7f121bd9c47564b6f5c283e3f2
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-24 22:24:29 +0530

    Update oops.java

--- oops.java
+++ oops.java
@@ -20,3 +20,39 @@ public class oops {
 		GFG.get(); 
 	} 
 } 
+
+
+//// Java program to Illustrate Creation of Object
+// Using new Instance
+
+// Main class
+class opps {
+
+	// Declaring and initializing string
+	String name = "Hello";
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Try block to check for exceptions
+		try {
+			Class cls = Class.forName("GFG");
+			// Creating object of main class
+			// using instance method
+			GFG obj = (opps)cls.newInstance();
+			// Print and display
+			System.out.println(obj.name);
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (InstantiationException e) {
+
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+
+			e.printStackTrace();
+		}
+	}
+}
