f506ee4bb0a2444de1553bdb8e4cd185e1ac107c
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-03-27 00:22:45 +0530

    Update LeetCode.java

--- LeetCode.java
+++ LeetCode.java
@@ -315,3 +315,25 @@ class Solution {
 
     }
 }
+
+//Duplicate Elements in array:
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> duplicates = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                duplicates.add(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        // Restore nums array (optional)
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+        }
+
+        return duplicates;
+    }
+}
