da3e98c91dabdc4d0225253ddfebe08208d8c249
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-04-07 21:29:10 +0530

    Update string.java

--- string.java
+++ string.java
@@ -9,3 +9,35 @@ public class String {
 		System.out.println(message);
 	}
 }
+
+//Naive Pattern Searching algorithm: 
+
+       public class HelloWorld{
+ 
+    static void search(String pat, String txt)
+    {
+        int l1 = pat.length();
+        int l2 = txt.length();
+        int i = 0, j = l2 - 1;
+ 
+        for (i = 0, j = l2 - 1; j < l1;) {
+ 
+            if (txt.equals(pat.substring(i, j + 1))) {
+                System.out.println("Pattern found at index "
+                                   + i);
+            }
+            i++;
+            j++;
+        }
+    }
+     
+      // Driver's code
+    public static void main(String args[])
+    {
+        String pat = "AABAACAADAABAAABAA";
+        String txt = "AABA";
+     
+          // Function call
+        search(pat, txt);
+    }
+}
