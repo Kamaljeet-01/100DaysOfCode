17bf0cb11323f944f98bfa820a6a17e89ff5acf4
Kamaljeet <kamaljeetkamaljeet548@gmail.com>
2024-05-03 22:22:31 +0530

    Update string.java

--- string.java
+++ string.java
@@ -117,3 +117,26 @@ class HelloWorld {
         }
     }
 }
+
+
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Override toString() to provide a custom string representation
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + "}";
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person("John", 30);
+        System.out.println(person.toString()); // Output: Person{name='John', age=30}
+    }
+}
